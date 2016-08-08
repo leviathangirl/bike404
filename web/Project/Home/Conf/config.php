653e030e09792945d549adca91331bfed766f994
catscarlet <i@catscarlet.com>
2016-08-08 23:48:37 +0800

    daily commit

--- web/Project/Home/Conf/config.php
+++ web/Project/Home/Conf/config.php
@@ -11,10 +11,11 @@ return array(
     'SITE_URL' => 'http://192.168.214.130/bike404/web/index.php/Home/',
     'IMG_PREFIX' => 'http://192.168.214.130/bike404/web/Public/temp/',
     //'IMGTHUMBNAIL_PREFIX'    => 'http://192.168.214.130/ImageThumbnail/index.php?img=',
-    'LASTMODIFIED_TIME' => 1464011964,
-    'BIKE404_VERSION' => 'v0.1.1-Alpha',
+    'LASTMODIFIED_TIME' => 1467909478,
+    'BIKE404_VERSION' => '0.1.4-Alpha',
     'META_KEYWORDS' => 'BIKE404,BIKE,404,BIKE404公益,自行车,被盗,公益,丢失,盗窃,开源,NotFound',
 
+    'POST_TMP' => '/tmp/',
     'URL_ROUTER_ON' => true,
     'URL_ROUTE_RULES' => array(
         array('sitemap.xml', 'tools/sitemap', null, array('method' => 'get')),
--- web/Project/Home/Controller/ReportController.class.php
+++ web/Project/Home/Controller/ReportController.class.php
@@ -10,4 +10,29 @@ class ReportController extends Controller
     {
         $this->redirect('About/index', null, 1, '重定向至[关于本站]页面中...');
     }
+
+    public function lost()
+    {
+        $this->display('report_lost');
+    }
+
+    public function clue()
+    {
+        $this->display('report_clue');
+    }
+
+    public function postlost()
+    {
+        $uploaddir = C('POST_TMP');
+        $uploaduserdir = '/';
+        $uploadfile = $uploaddir . basename($_FILES['userfile']['name']);
+        echo nl2br(print_r($_FILES, true));
+        echo nl2br(print_r($uploadfile, true));
+        echo nl2br(print_r(I(), true));
+    }
+
+    public function postclue()
+    {
+
+    }
 }
