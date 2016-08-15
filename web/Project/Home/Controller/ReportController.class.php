0682f65763d9035f2a2cd7bc24b4977c04bed245
catscarlet <i@catscarlet.com>
2016-08-15 23:45:22 +0800

    daily commit

--- web/Project/Home/Controller/ReportController.class.php
+++ web/Project/Home/Controller/ReportController.class.php
@@ -8,7 +8,8 @@ class ReportController extends Controller
 {
     public function index()
     {
-        $this->redirect('About/index', null, 1, '重定向至[关于本站]页面中...');
+        //$this->redirect('About/index', null, 1, '重定向至[关于本站]页面中...');
+        $this->display();
     }
 
     public function lost()
