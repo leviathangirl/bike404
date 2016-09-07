0d9182459eb2b9f3b7af6d13ab961eff7d3aa4ba
catscarlet <i@catscarlet.com>
2016-09-08 00:04:09 +0800

    daily commit

--- web/Public/js/report.js
+++ web/Public/js/report.js
@@ -1,3 +1,5 @@
+var timestamp;
+
 $(document).ready(function() {
     $('.datepicker').pickadate({
         selectMonths: true, // Creates a dropdown to control month
@@ -6,18 +8,17 @@ $(document).ready(function() {
 
     $('select').material_select();
     $('#uploadimg').on('click', function() {
-        //console.log('click');
         var files = document.getElementById('files').files;
         upload(files);
+    });
+    $('#test').on('click', function() {
+        postReportLostData(1);
     });
 });
 
 function upload(files) {
-    //var mainform = document.getElementById('mainform');
     var formData = new FormData();
     formData.append('posttest', 'post files');
-
-
 
     console.log(files);
     console.log(files.length);
@@ -37,10 +38,10 @@ function upload(files) {
     xhr.onreadystatechange = function() {
         if (xhr.readyState == 4 && xhr.status == 200) {
             console.log(xhr.responseText);
+            timestamp = xhr.responseText;
+            postInfo(timestamp);
         } else {
-            //console.log(xhr.statusText);
         }
-        //console.log(xhr);
     };
 }
 
@@ -49,4 +50,30 @@ function updateProgress(event) {
         var percentComplete = event.loaded / event.total;
         console.log(percentComplete);
 　　　　}
+}
+
+function postReportLostData(timestamp) {
+    if (!timestamp) {
+        console.log('timestamp is false.');
+        return false;
+    }
+    reportlostdata = new FormData(document.getElementById('reportlostform'));
+    reportlostdata.append('timestamp', timestamp);
+    console.log(reportlostdata);
+
+    $.ajax({
+        type: 'POST',
+        url: '/post_test.php',
+        data: reportlostdata,
+        processData: false,  // 告诉jQuery不要去处理发送的数据
+        contentType: false,   // 告诉jQuery不要去设置Content-Type请求头
+        dataType: 'json',
+        async: false,
+        success: function(msg) {
+            console.log(msg);
+        },
+        error: function() {
+            console.log(msg);
+        }
+    });
 }
